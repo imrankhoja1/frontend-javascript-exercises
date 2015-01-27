074ce2af9f8b151c38b008fe70614538a05691fc
Imran Khoja <imrankhoja1@gmail.com>
2015-01-26 20:27:11 -0500

    finish functions

--- 06-functions/00-basics/challenge.js
+++ 06-functions/00-basics/challenge.js
@@ -1,3 +1,3 @@
-module.exports.helloWorld = undefined;
+module.exports.helloWorld = function() { return 'Hello World'; }
 
-module.exports.multiply = undefined;
+module.exports.multiply = function( num1, num2 ) { return num1 * num2; }
