42ca1fb1609c6c8211a1db9fd14e9b0ff623e8d6
vagrant <vagrant@precise32.(none)>
2015-01-27 16:51:03 +0000

    finish arrays

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,20 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function( array ) {
+  var result = [];
+  for (var i = (array.length - 1); i >= 0; i--) {
+    result.push(array[i]);
+  }
+  result.unshift(1);
+  return result; 
+};
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function ( array ) {
+  var result = "";
+  for (var i = 0; i < array.length; i++) {
+    result += array[i] + "+";
+  }
+  return result.substring(0, result.length - 1);
+};
 
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.arrayQuantityPlusOne = function ( array ) {
+  return array.length + 1;
+};
--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,17 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function ( item, list ) {
+  if (list.indexOf(item) == -1) {
+    list.push(item);
+    return list;
+  } else {
+    return list;
+  }
+};
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function ( array ) {
+  array = array.sort();
+  var result = [];
+  while (array.length > 0) {
+    result.push(array.pop());
+  }
+  return result;
+};
